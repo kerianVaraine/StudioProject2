0a544e6c9e8d24e97cfd812906807550f3641255
Kay Baldock <balka799@student.otago.ac.nz>
2019-08-08 11:55:22 +1200

    multi-coloured buttons

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ window.onload = setInterval(clock,1000);
 
 function clock() {
     var dt = new Date();
-        
+   
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     var ldate = (days[dt.getDay()]+ " " + dt.getDate() + " "  + months[dt.getMonth()] + " " + dt.getFullYear());
