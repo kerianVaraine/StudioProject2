a6e76d1a0832355881ae2bd062f9cbd4b737e3fe
kerianVaraine <chambervulgarus@gmail.com>
2019-08-07 14:19:43 +1200

    SPWA approach

--- js/populateContent.js
+++ js/populateContent.js
@@ -1,5 +1,9 @@
-let synth = window.speechSynthesis;
+//Main var for subcategory listing
+let subCategory = "basic";
 
+//Text-to-Speech, browser based.
+let synth = window.speechSynthesis;
+//BUTTON POPULATION SCRIPTS
 //parse content from json file, then populate page with buttons from the selected section.
 // From:
 //https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
@@ -15,20 +19,24 @@ request.send();
 request.onload = function () {
     phrases = request.response;
 }
-
-
 // end of json importing
-//
-//
+
+
+
 // adds button with all functionality and name
-let getButtonText = function (pCat, index) {
+let getButtonText = function (pCat, index, categoryName) {
     let button = document.createElement("button"); //create button element
     button.innerText = pCat[index][0]; //add button text from json file
     button.className = "entry"; //assign class/css/styling
-    button.onclick = function () { //onclick functionality
+    //BUTTON ON CLICK
+    button.onclick = function () {
         synth.speak(new SpeechSynthesisUtterance(pCat[index][1])); //speak phrase from json
+        subCategory = pCat[index][0];//set subCategory global variable to populate the right buttons.
         removeEntries(); //remove buttons once clicked
-        document.getElementsByClassName("section")[0].focus(); //return focus to categories
+        if(categoryName != "basic") {
+            getPage("phrases");
+        }
+        //document.getElementsByClassName("section")[0].focus(); //return focus to categories Messes with things.
     };
     document.getElementById("entries").appendChild(button); //adds all butons to page.
 }
@@ -38,7 +46,7 @@ let populateEntries = function (category) {
     // make reference to json.category;
     let pCat; // ref to json category array
     let arrLength;
-    // this selects the catagory inside the json file to loop through and populate buttons
+    // this selects the category inside the json file to loop through and populate buttons
     switch (category) {
         case ("basic"):
             pCat = phrases.basic;
@@ -57,9 +65,9 @@ let populateEntries = function (category) {
             break;
     }
     arrLength = pCat.length;
-    //loop to populate page with json info
+    //loop to populate page with json info, and main category name for page choice
     for (let i = 0; i < arrLength; i++) {
-        getButtonText(pCat, i);
+        getButtonText(pCat, i, category);
     }
 }
 
@@ -73,27 +81,46 @@ function removeEntries() {
 
 ///Page specific, using ids of section buttons to create phrase buttons
 
-//create array of section buttons (ie: top nav)
-const sectionButtons = document.getElementsByClassName("section");
-const secButtArr = Array.from(sectionButtons);
-//apply onclick funtion to each button
-for (let i = 0; i < secButtArr.length; i++) {
-    secButtArr[i].onfocus = function () {
-        removeEntries();
-        populateEntries(secButtArr[i].id);
-    };
-    secButtArr[i].onclick = function () {
-        removeEntries();
-        populateEntries(secButtArr[i].id);
-        document.getElementsByClassName("entry")[0].focus(); //focus on first phrase button
+let populateButtons = function () {
+    //create array of section buttons (ie: top nav)
+    const sectionButtons = document.getElementsByClassName("section");
+    const secButtArr = Array.from(sectionButtons);
+    //apply onclick funtion to each button
+    for (let i = 0; i < secButtArr.length; i++) {
+        secButtArr[i].onfocus = function () {
+            removeEntries();
+            populateEntries(secButtArr[i].id);
+            console.log(secButtArr[i].id);
+        };
+        secButtArr[i].onclick = function () {
+            removeEntries();
+            populateEntries(secButtArr[i].id);
+            document.getElementsByClassName("entry")[0].focus(); //focus on first phrase button
+        }
     }
 }
 
-//assign shortcuts button 
-// document.getElementById("assignShortCut").onclick = function () {
-//     if (leftKey == undefined) {
-//         assignShortcut();
-//     }
-//     document.getElementById("assignShortCut").innerText = "reset shortcuts";
-//     resetShortcuts();
-// }
+
+////////////////////////////
+//Page loading content stuff
+////////////////////////////
+//parameter is button id, for checking if home page.
+// 2nd param is name of sub category chosen.
+
+let getPage = function (pageName) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("SPAContainer").innerHTML =
+                this.responseText;
+                if(document.getElementById("subCategory")){
+                document.getElementById("subCategory").innerHTML = subCategory;
+
+                }
+                populateButtons();
+        }
+    };
+    xhttp.open("GET", "./pages/" + pageName + ".html", true);
+    //sets page var for getting sub category
+    xhttp.send();
+}
