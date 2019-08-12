f3daf1e4e01c6d34bcd124514658aebc4013d28b
kerianVaraine <chambervulgarus@gmail.com>
2019-08-13 09:19:14 +1200

    header gridify

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ function clock() {
    
     var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var ldate = (days[dt.getDay()]+ " " + dt.getDate() + " "  + months[dt.getMonth()] + " " + dt.getFullYear());
+    var ldate = (days[dt.getDay()]+ "<br> " + dt.getDate() + " "  + months[dt.getMonth()] + " " + dt.getFullYear());
     var hours = dt.getHours();
     var mins = (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
     var time = hours + ":" + mins;
